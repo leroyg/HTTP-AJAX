e9e31a1f4ea9648665d8a4060589320099e933c2
leroyg <leroy.gardner@gmail.com>
2019-06-30 10:59:30 -0500

    building out friend.js

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -5,22 +5,39 @@ import axios from 'axios';
 
 
 class Friend extends React.Component {
-    
+    deleteFriend = () => {
+        const { id } = this.props.friend;
+
+        axios
+            .delete(`http://localhost:5000/friends/${id}`)
+            .then(response => this.props.updateFriends(response.data))
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
     render () {
         const { name, age, email, id } = this.props.friend;
-        const { id } = this.props.friend;
 
         return(
             <Card className="friendCard">
-                <div className="friendHeaderDiv">
+                <div className="friend-header">
                     <h2>{name}</h2>
                 </div>
                 <div>
                     <p>Age: {age}</p>
                     <p>Email: {email}</p>
-                </div>   
-            </Card>
+                </div> 
 
+                <div className="change-friend">
+                    <Link to={`/friend/edit/${id}`} className='friend-card-edit-text'>
+                    <i className='small material-icons'>edit</i>
+                    </Link>
+                    <span onClick={this.deleteFriend} className='delete-button'>
+                        <i className="small material-icons">Delete</i>
+                    </span>
+                </div>  
+            </Card>
         );
     }
 
